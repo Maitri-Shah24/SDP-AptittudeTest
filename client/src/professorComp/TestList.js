9a87fcabb99f325b781978595efe2d05d690a1d5
Krisha Thumar <krishathumar2308@gmail.com>
2024-04-27 11:24:19 +0530

    testlist

--- client/src/professorComp/TestList.js
+++ client/src/professorComp/TestList.js
@@ -94,12 +94,14 @@ export default function TestList(){
       <TabPanel >
       <div className="test-card-container">
         {currentTests.map(test => (
+          <div key={test._id} to={`/instruction/${test._id}`} className='test-card'>
           <div key={test._id}>
             <h3>{test.testName}</h3>
             <p>Duration: {test.duration} minutes</p>
             <p>Date: {test.date}</p>
             <p>Start Time: {test.startTime}</p>
             <p>End Time: {test.endTime}</p>
+          </div>
           </div>
         ))}
       </div>
